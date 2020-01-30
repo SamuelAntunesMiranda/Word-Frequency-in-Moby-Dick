2131ad3004fac747ce5a7d416d95eb70a2b63878
Samuel Antunes Miranda <57368369+SamuelAntunesMiranda@users.noreply.github.com>
2020-01-30 01:39:04 -0300

    Update MobyDick.py

--- MobyDick.py
+++ MobyDick.py
@@ -8,22 +8,25 @@ r.encoding = 'utf-8'
 html = r.text
 soup = BeautifulSoup(html, 'html.parser')
 text = soup.get_text()
-#creating an array that only has the words, removing spaces, commas etc.
+
+#creating an array that only has words, removing spaces, commas etc.
 tokenizer = nltk.tokenize.RegexpTokenizer('\w+')
 tokens = tokenizer.tokenize(text)
 words = []
+
 #making every word lowercase
 for word in tokens:
     words.append(word.lower())
+    
 #getting the stop words for the english language
 sw = nltk.corpus.stopwords.words('english')
 words_ns = []
+
 #creating an array without the stop words
 for word in words:
     if word not in sw:
         words_ns.append(word)
 
-#using nltk frequency distribution function to get the most commom word in the array
-#without the stop words.
+#using nltk frequency distribution function to get the most commom word in the array without the stop words.
 freqdist = nltk.FreqDist(words_ns)
 print(freqdist.most_common(1))
